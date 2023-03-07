34579d4b57aa8df98102fda448a17d02962d8d60
Fábio Machado de Oliveira <machado2@gmail.com>
2023-03-07 10:39:15 -0300

    update

--- sites/test-site/src/main.jsx
+++ sites/test-site/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Physicstree } from 'react-three-fiber-trees'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Physicstree />
   </React.StrictMode>,
